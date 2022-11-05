99f617c47527b794bdc5c7a7316d9060061f05b9
Rafik <rafik.remal@r-bim.fr>
2022-11-05 09:02:18 +0100

    Add project files.

---
+++ RevitAddin_Session1Challenge/Command01_Challenge.cs
@@ -0,0 +1,43 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+#endregion
+
+namespace RevitAddin_Session1Challenge
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command01_Challenge : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            try
+            {
+
+                TaskDialog.Show("test", "Hello World !!!!");
+
+
+                return Result.Succeeded;
+
+            }
+
+            catch (Exception e)
+
+            {
+
+              string Message = e.Message;
+
+                return Result.Failed;   
+            }  
+        }
+    }
+}
